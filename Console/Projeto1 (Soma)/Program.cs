3f5e112eb445481ccdfdefdef05d66fd34f089ef
Caique Santos <caisbrito2003@gmail.com>
2020-11-12 20:13:25 -0300

    Projeto1 Console realizado

---
+++ Console/Projeto1 (Soma)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Digite o primeiro número:");
+            string num1_text = Console.ReadLine();
+            int num1 = int.Parse(num1_text);
+
+            Console.WriteLine("Digite o segundo número:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+
+            Console.Write("O resultado é: ");
+            Console.WriteLine(soma);
+        }
+    }
+}
