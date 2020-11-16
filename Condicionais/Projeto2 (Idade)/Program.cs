069745fc94c7cecde67197e8ebbfe90652eea40a
Caique Santos <caisbrito2003@gmail.com>
2020-11-16 10:48:20 -0300

    Condicional idade feita

---
+++ Condicionais/Projeto2 (Idade)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Projeto2__Idade_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Bem vindo ao calculador de idade");
+            Console.WriteLine("--------------------------------");
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------");
+
+            int meses = idade * 12;
+            int dias = idade * 365;
+            int horas = dias * 24;
+            int minutos = horas * 60;
+
+            Console.WriteLine("Você viveu:");
+            Console.WriteLine(meses+" meses");
+            Console.WriteLine(dias+" dias");
+            Console.WriteLine(horas+" horas");
+            Console.WriteLine(minutos+" minutos");
+        }
+    }
+}
