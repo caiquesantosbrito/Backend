7f61cae077bc5413f8187b97699f03f096e39f2c
Caique Santos <caisbrito2003@gmail.com>
2020-11-16 12:14:15 -0300

    Condicional nascimento feita

---
+++ Condicionais/Projeto3 (Nascimento)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Projeto3__Nascimento_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("----------------------------------");
+            Console.Write("Digite o ano em que nasceu: ");
+            int ano_que_nasceu = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+            Console.Write("Digite o ano atual: ");
+            int ano_atual = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+
+            int anos = ano_atual - ano_que_nasceu;
+            int semanas = anos * 52;
+
+            Console.WriteLine("Você tem:");
+            Console.WriteLine(anos+" anos"); 
+            Console.WriteLine("Você viveu:");
+            Console.WriteLine(semanas+" semanas");
+        }
+    }
+}
