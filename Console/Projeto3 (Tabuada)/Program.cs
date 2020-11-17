e64b648defbdf12db07748b4b02f5c55f5c938b2
Caique Santos <caisbrito2003@gmail.com>
2020-11-17 20:57:10 -0300

    Tabuada feita

---
+++ Console/Projeto3 (Tabuada)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto3__Tabuada_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja ver a tabuada de qual número?");
+            int resposta = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------");
+
+            int resultado;
+
+            for(int contador = 10; contador >=0; contador --){
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+            }
+        }
+    }
+}
