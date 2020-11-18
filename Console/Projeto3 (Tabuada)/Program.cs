72b7212d15596adc52451d145a17e6180c4bb4c3
Caique Santos <caisbrito2003@gmail.com>
2020-11-18 14:26:48 -0300

    Partes extras adicionadas

--- Console/Projeto3 (Tabuada)/Program.cs
+++ Console/Projeto3 (Tabuada)/Program.cs
@@ -9,12 +9,14 @@ namespace Projeto3__Tabuada_
             Console.WriteLine("Deseja ver a tabuada de qual número?");
             int resposta = int.Parse(Console.ReadLine());
             Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Os valores da sua tabuada são:");
 
             int resultado;
 
             for(int contador = 10; contador >=0; contador --){
                 resultado = resposta * contador;
                 Console.WriteLine($"{resposta} * {contador} = {resultado}");
+                System.Threading.Thread.Sleep(800);
             }
         }
     }
